a270088a69b91af64e84b93814f6265821727714
Ria Garlapadu <rbunrsq@gmail.com>
2025-07-09 13:14:53 -0400

    Initial Commit

---
+++ InteractiveUITests/InteractiveUITests.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUITests.swift
+//  InteractiveUITests
+//
+//  Created by roby on 7/9/25.
+//
+
+import Testing
+@testable import InteractiveUI
+
+struct InteractiveUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
