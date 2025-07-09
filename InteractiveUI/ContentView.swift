c0262b478bedf93088271229485f77105de4487f
Ria Garlapadu <rbunrsq@gmail.com>
2025-07-09 14:37:00 -0400

    Added @State property wrapper and action within the button to change the text when user inputs name

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
     var body: some View {
         VStack {
             Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
+            
+            TextField("Type your name here...", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+            
+            Button("Submit name") {
+            }
+                .font(.title)
+                .border(Color.green, width: 1)
+                .padding()
+            
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
+    
+    #Preview {
+        ContentView()
+    }
+    
 }
